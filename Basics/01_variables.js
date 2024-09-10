e00a28ea0199aa97ad816d28ed72ead72d1dd0dd
adtiya-tech <adityaannu162@gmail.com>
2024-09-10 05:54:46 +0000

    variables

---
+++ Basics/01_variables.js
@@ -0,0 +1,35 @@
+// Javascript Variables
+
+// console.log() -- To display the data in terminal
+
+// There are 3 ways to declare a variable in javascript--
+
+//1. var -- not used anymore
+//2. let -- to declare a variable
+//3. const -- to create a constant value
+
+let firstName = "avinash"
+
+// console.log(firstName)
+
+
+let age = 32
+
+// console.log(age);
+
+firstName = "atul"
+
+// console.log(firstName)
+
+// let will allow you to create a variable which can change it's values whenever the user want
+
+const lastName = "dubey"
+
+console.log(lastName);
+
+lastName = "sharma"
+
+console.log(lastName);
+
+// const will never allow you to change a value of the variable
+
